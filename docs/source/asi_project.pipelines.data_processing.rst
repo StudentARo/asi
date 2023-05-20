9c262f71d215deefdf7db4d0921fb6c8d5ff2083
Trojan1244 <trojan12445@gmail.com>
2023-05-20 14:57:06 +0200

    Pipeline modularization

---
+++ docs/source/asi_project.pipelines.data_processing.rst
@@ -0,0 +1,26 @@
+asi\_project.pipelines.data\_processing package
+===============================================
+
+.. automodule:: asi_project.pipelines.data_processing
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Submodules
+----------
+
+asi\_project.pipelines.data\_processing.nodes module
+----------------------------------------------------
+
+.. automodule:: asi_project.pipelines.data_processing.nodes
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+asi\_project.pipelines.data\_processing.pipeline module
+-------------------------------------------------------
+
+.. automodule:: asi_project.pipelines.data_processing.pipeline
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- src/asi_project/pipelines/data_processing/pipeline.py
+++ src/asi_project/pipelines/data_processing/pipeline.py
@@ -4,21 +4,27 @@ generated using Kedro 0.18.8
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from kedro.pipeline.modular_pipeline import pipeline
+
 from .nodes import preprocess_flights
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-                func=preprocess_flights,
-                inputs="flights_train",
-                outputs="preprocessed_flights_train",
-                name="preprocess_flights_train_node",
-            ),
+    return pipeline(
+        [
             node(
-                func=preprocess_flights,
-                inputs="flights_test",
-                outputs="preprocessed_flights_test",
-                name="preprocess_flights_test_node",
-            ),
-    ])
+                    func=preprocess_flights,
+                    inputs="flights_train",
+                    outputs="preprocessed_flights_train",
+                    name="preprocess_flights_train_node",
+                ),
+            node(
+                    func=preprocess_flights,
+                    inputs="flights_test",
+                    outputs="preprocessed_flights_test",
+                    name="preprocess_flights_test_node",
+                ),
+        ],
+        namespace = "data_processing",
+        inputs = ["flights_train", "flights_test"],
+        outputs = ["preprocessed_flights_train", "preprocessed_flights_test"])
